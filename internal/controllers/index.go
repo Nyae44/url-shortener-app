28709d503e3fc5431f14604e47825381cab5117f
Nyae44 <tingadaley@gmail.com>
2024-11-18 18:00:50 +0300

    style index.html

--- internal/controllers/index.go
+++ internal/controllers/index.go
@@ -1,1 +1,18 @@
 package controllers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func ShowIndex(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("internal/views/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err = temp.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
